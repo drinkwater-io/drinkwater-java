27276313131cfb369b0bdc4d554abd05488a654c
Cedric Dumont <cedric.dumont.ext@atos.net>
2016-12-29 15:05:52 +0100

    code lifting

--- components/drinkwater-core/src/main/java/drinkwater/core/DrinkWaterApplication.java
+++ components/drinkwater-core/src/main/java/drinkwater/core/DrinkWaterApplication.java
@@ -22,21 +22,12 @@ import java.util.stream.Stream;
 //@Vetoed
 public class DrinkWaterApplication {
 
-//    @Inject
-//    @DrinkWaterApplicationConfig
-//    private Instance<Object> applicationConfig;
-
-//    public java.util.List<CamelContext> get_camelContexts() {
-//        return _camelContexts;
-//    }
-
     Map<Class, ProducerTemplate> producertemplates = new HashMap<>();
 
     List<ServiceConfigurationBuilder> serviceBuilders = List.empty();
 
     java.util.List<CamelContext> _camelContexts = new ArrayList<>();
 
-//    @PostConstruct
     public void start() {
 
         for (ServiceConfigurationBuilder builder :serviceBuilders) {
@@ -46,8 +37,6 @@ public class DrinkWaterApplication {
         }
     }
 
-    // be sure to stop camel context on bean destroy
-//    @PreDestroy
     public void stop() {
         for (CamelContext ctx : _camelContexts) {
             try {
@@ -61,38 +50,6 @@ public class DrinkWaterApplication {
     public void addServiceBuilder(ServiceConfigurationBuilder builder){
         serviceBuilders = serviceBuilders.append(builder);
     }
-
-//    private List<ServiceConfiguration> getServiceConfigurations(){
-//        Object applicationConfigObject =  applicationConfig.get();
-//
-//        DrinkWaterApplicationConfig configAnnotation = applicationConfigObject.getClass().getAnnotation(DrinkWaterApplicationConfig.class);
-//
-//        java.util.Set<Method> methods = getMethods(applicationConfigObject.getClass(),
-//                withReturnType(ServiceConfigurationCollection.class));
-//
-//        if(methods.size() == 0){
-//            throw new RuntimeException(String.format("The type annotated with @DrinkWaterApplicationConfig, " +
-//                            "should provide a method with signature public %s %s()", configAnnotation.configurationMethod(),
-//                    ServiceConfiguration.class.getName())
-//            );
-//        }
-//
-//        List<ServiceConfiguration> configs = List.empty();
-//
-//        configs = List.ofAll(methods).
-//                map(
-//                        m -> {
-//                            try {
-//                                return (ServiceConfigurationCollection)m.invoke(applicationConfigObject);
-//                            } catch (Exception e) {
-//                                throw new RuntimeException(String.format(
-//                                        "Error while configuring Application. call to method %s.%s failed", m.getDeclaringClass(),m.getName()));
-//                            }
-//                        })
-//                .flatMap(col -> col.getConfigurations());
-//
-//        return configs;
-//    }
 
     private void createCamelContextFromConfig(ServiceConfiguration config) {
         try {
