b9978649a01c09e678619a537c890acd3dbc3251
Cedric Dumont <cedric.dumont.ext@atos.net>
2017-01-04 06:33:58 +0100

    before refactoring services configuration

---
+++ components/drinkwater-core/src/main/java/drinkwater/core/reflect/MockInvocationHandler.java
@@ -0,0 +1,29 @@
+package drinkwater.core.reflect;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by A406775 on 3/01/2017.
+ */
+public class MockInvocationHandler implements InvocationHandler {
+
+    private final Object target;
+
+    //FIXME : review how the invocation is done for direct object access.
+    public MockInvocationHandler(Object bean) {
+        try {
+            this.target = bean;
+        } catch (Exception e) {
+            throw new RuntimeException("could not create bean");
+        }
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+
+        return method.invoke(target, args);
+    }
+}
+
