36e2416706a1615a051b24ccc8a957bcb331ca7d
Cedric Dumont <cedric.dumont.ext@atos.net>
2017-01-16 14:13:17 +0100

    remplace mock-server with wiremock and updated the jackson libraries

--- components/drinkwater-helper/src/main/java/drinkwater/helper/json/CustomJacksonObjectMapper.java
+++ components/drinkwater-helper/src/main/java/drinkwater/helper/json/CustomJacksonObjectMapper.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+//import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 
 /**
  * Created by A406775 on 3/01/2017.
@@ -12,7 +12,8 @@ import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 public class CustomJacksonObjectMapper extends ObjectMapper {
 
     public CustomJacksonObjectMapper() {
-        this.registerModule(new JSR310Module());
+        //TODo fix this issue on deprecetion
+        //this.registerModule(new JSR310Module());
         this.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         this.enable(SerializationFeature.INDENT_OUTPUT);
--- examples/drinkwater.examples.remote/src/test/java/test/drinkwater/examples/remote/MultiServiceRemoteTest.java
+++ examples/drinkwater.examples.remote/src/test/java/test/drinkwater/examples/remote/MultiServiceRemoteTest.java
@@ -1,5 +1,6 @@
 package test.drinkwater.examples.remote;
 
+import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import drinkwater.IServiceConfiguration;
 import drinkwater.ServiceConfiguration;
@@ -9,12 +10,15 @@ import drinkwater.examples.multiservice.IServiceD;
 import drinkwater.examples.remote.MultiServiceRemoteConfiguration;
 import drinkwater.test.HttpUnitTest;
 import org.junit.*;
-import org.mockserver.client.server.MockServerClient;
-import org.mockserver.junit.MockServerRule;
-import org.mockserver.model.HttpResponse;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.junit.Assert.assertEquals;
-import static org.mockserver.model.HttpRequest.request;
+
+//import org.mockserver.client.server.MockServerClient;
+//import org.mockserver.junit.MockServerRule;
+//import org.mockserver.model.HttpResponse;
+//import static org.mockserver.model.HttpRequest.request;
 
 /**
  * Unit test for simple App.
@@ -23,15 +27,12 @@ public class MultiServiceRemoteTest extends HttpUnitTest {
 
     static DrinkWaterApplication app;
 
-    static {
-        //FIXME manage the logging system it's shity
-        System.setProperty("logback.configurationFile", "mockserver_logback.xml");
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "info");
-    }
+    @ClassRule
+    public static WireMockClassRule wireMockRule = new WireMockClassRule(
+            options().dynamicPort());
 
     @Rule
-    public MockServerRule mockServerRule = new MockServerRule(this);
-    private MockServerClient mockServerClient;
+    public WireMockClassRule instanceRule = wireMockRule;
 
     @BeforeClass
     public static void setup() throws Exception {
@@ -39,6 +40,7 @@ public class MultiServiceRemoteTest extends HttpUnitTest {
         MultiServiceRemoteConfiguration config = new MultiServiceRemoteConfiguration();
         app.addServiceBuilder(config);
         app.start();
+
     }
 
     @AfterClass
@@ -47,24 +49,20 @@ public class MultiServiceRemoteTest extends HttpUnitTest {
     }
 
     @Before
-    public void setExpectations() {
-        mockServerClient.when(
-                request()
-                        .withMethod("GET")
-                        .withPath("/serviceD/uppercaseData")
-                        .withQueryStringParameter("data", "someData")
-        ).respond(
-                HttpResponse.response().withBody("mocked Data from D")
-        );
+    public void setExpectation(){
+        //Sets the expectation for wiremock
+        stubFor(get(urlEqualTo("/serviceD/uppercaseData?data=someData"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("mocked Data from D")));
     }
-
 
     @Test
     public void ShouldCallRemoteService() throws UnirestException {
 
         //TODO change the property setting using a constant
         IServiceConfiguration config = (IServiceConfiguration)
-                ServiceConfiguration.empty().withProperty("drinkwater.rest.port", mockServerRule.getPort()).asRemote();
+                ServiceConfiguration.empty().withProperty("drinkwater.rest.port", wireMockRule.port()).asRemote();
 
         app.patchService("serviceD", config);
 
@@ -80,7 +78,7 @@ public class MultiServiceRemoteTest extends HttpUnitTest {
 
         //TODO change the property setting using a constant
         IServiceConfiguration config = (IServiceConfiguration)
-                ServiceConfiguration.empty().withProperty("drinkwater.rest.port", mockServerRule.getPort()).asRemote();
+                ServiceConfiguration.empty().withProperty("drinkwater.rest.port", wireMockRule.port()).asRemote();
 
         //not remote
         IServiceA serviceA = app.getService("serviceA");
@@ -100,18 +98,4 @@ public class MultiServiceRemoteTest extends HttpUnitTest {
         assertEquals("A -> [B -> [C -> [servicCConn : someData] - mocked Data from D]]", transformed);
     }
 
-//    @Test
-//    public void TestTracing() throws UnirestException {
-//
-//        IServiceConfiguration config = (IServiceConfiguration)
-//                ServiceConfiguration.empty().withProperty("drinkwater.rest.port", mockServerRule.getPort()).asRemote();
-//
-//        app.patchService("serviceD", config);
-//
-//        String result = httpGetString("http://localhost:8889/serviceA/data?data=someData").result();
-//
-//        assertEquals("A -> [B -> [C -> [servicCConn : someData] - mocked Data from D]]", result);
-//
-//        //assertEquals(2, app.getEventAggregator().currentSize());
-//    }
 }
